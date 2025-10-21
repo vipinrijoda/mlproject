322f62d02c0616bc5293496aa97c0d1dfdff3d4c
vipin yadav <vipinyadavrijoda9131@gmail.com>
2025-10-21 16:11:24 +0530

    Data Ingestion

--- source/logger.py
+++ source/logger.py
@@ -2,14 +2,22 @@ import logging
 import os
 from datetime import datetime
 
+# Log file name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+# Log directory - create logs directory first
+logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
+# Log file path - then join with filename
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+# Logger configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+# Create and export logger
+logger = logging.getLogger("mlproject_logger")
