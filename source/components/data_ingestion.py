13e2d45cfd008fae56340f8c2ef5fda51a18cc7c
vipin yadav <vipinyadavrijoda9131@gmail.com>
2025-10-21 18:19:25 +0530

    Data Transformation done

--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -12,6 +12,9 @@ from source.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from source.components.data_transformation import DataTransformation
+from source.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts',"train.csv")
@@ -57,4 +60,7 @@ class DataIngestion:
 
 if __name__=="__main__":
      obj=DataIngestion()
-     obj.initiate_data_ingestion()
+     train_data,test_data=obj.initiate_data_ingestion()
+
+     data_transformation=DataTransformation()
+     data_transformation.initiate_data_transformation(train_data,test_data)
